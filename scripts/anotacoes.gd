d1d8c932c0e9a0647959f56b1ae4fe7a745d2550
estelaGamba <sparren.contato@gmail.com>
2025-06-21 23:15:20 -0300

    Add files via upload

---
+++ scripts/anotacoes.gd
@@ -0,0 +1,13 @@
+extends Node
+#apenas para anotar algumas coisas
+
+#a tipagem recomendada do godot é a snake case
+#palavra começa com letra minúscula e se for duas palavras se separa com underline
+
+
+	#funçoes
+#as funções são blocos de codigo
+#praticamente o tipo de função é o que a pessoa escolhe
+#tambem é possivel escolher o nome que a funçao tera, mas existem tipos 
+#que ja vem junto com com o godot
+ 
